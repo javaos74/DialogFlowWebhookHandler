dd07c1aaa6892f86280e486c2da63acc4aafe0d5
Hyungsoo Kim <hyungsoo.kim@uipath.com>
2019-10-07 21:16:57 +0900

    Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -1,41 +1,62 @@
 import requests
-
+import json
 
 class Orchestrator:
-    token = None
-    startJobs = ''
-    Jobs = ''
 
-    def __init__(self, tenant, user, password, url='https://uipath.myrobots.co.kr/'):
-        self.url = url
-        self.token = self.__getToken(tenant, user, password)
-        self.startJobs = 'odata/Jobs/UiPath.Server.Configuration.OData.StartJobs'
-        self.Jobs = 'odata/Jobs'
+    def __init__(self, token_file):
+        self.token = ''
+        self.tenantName = ''
+        with open(token_file) as f:
+            tokens = json.loads(f.read())
+            self.idToken = tokens['id_token']
+            self.refresh_token = tokens['refresh_token']
+        self.getLogicalAccountName()
+        self.getServiceInstanceLogicalName()
+        self.refreshToken()
 
+    def refreshToken(self):
+        url ='https://account.uipath.com/oauth/token'
+        payload = {
+            "grant_type": "refresh_token",
+            "client_id": "5v7PmPJL6FOGu6RB8I1Y4adLBhIwovQN",
+            "refresh_token":self.refresh_token 
+        }
+        resp = requests.request('POST', url, data=json.dumps(payload), headers={'Content-Type': 'application/json'}).json()
+        self.token = resp['access_token']
+        return resp
 
-    def __getToken(self, tenant, user, password):
-        res = self.request('POST', 'api/account/authenticate',
-                           {'tenancyName': tenant,
-                            'usernameOrEmailAddress': user,
-                            'password': password})
-        print(res)
+    def getLogicalAccountName(self):
+        url = 'https://platform.uipath.com/cloudrpa/api/getAccountsForUser'
+        hdr = {'Authorization': 'Bearer ' + self.idToken}
+        resp = requests.request('GET', url, headers=hdr).json()
+        self.accountLogicalName = resp['accounts'][0]['accountLogicalName']
+        return resp['accounts'][0]['accountLogicalName']
 
-        return res["result"]
-
+    def getServiceInstanceLogicalName(self):
+        url = "https://platform.uipath.com/cloudrpa/api/account/%s/getAllServiceInstances" %(self.accountLogicalName)
+        hdr = {'Authorization': 'Bearer ' + self.idToken}
+        resp = requests.request('GET', url, headers=hdr).json()
+        print(resp)
+        self.tenantName = resp[0]['serviceInstanceLogicalName']
+        self.url = 'https://platform.uipath.com/%s/%s/' %(self.accountLogicalName, self.tenantName)
+        return resp[0]['serviceInstanceLogicalName']
 
     def request(self, type, extension, body=None):
         uri = self.url + extension
-        headers = self.__getHeaders(extension)
-
-        response = requests.request(type.upper(), uri, data=body, headers=headers)
-
-        return response.json()
+        hdr = self.__getHeaders(extension)
+        print(uri)
+        print(hdr)
+        resp = requests.request(type.upper(), uri, data=body, headers=hdr).json()
+        return resp
 
     def __getHeaders(self, extension):
-        if extension == self.startJobs:
-            headers = {'Authorization': 'Bearer ' + str(self.token),
-                       'Content-type': 'application/json'}
-        else:
-            headers = {'Authorization': 'Bearer ' + str(self.token) or ''}
+        hdr = {'Authorization': 'Bearer ' + str(self.token),
+                        'X-UIPATH-TenantName' : self.tenantName,
+                       'Accept': 'application/json'}
+        return hdr 
 
-        return headers
+
+if __name__ == '__main__' : 
+    orch = Orchestrator('token.json')
+    resp = orch.request('GET', 'odata/Robots')
+    print(resp)
